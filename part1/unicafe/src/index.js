9080a38b9b6542fb75d9b4bd34ba53dd09e15001
IngJheyner <david-ibagon@hotmail.com>
2023-11-11 15:12:18 -0500

    Unicafe part 1.8, paso 3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,23 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 
+const Statistics = ({good, neutral, bad}) => {
+
+  const average = (good - bad) / (good + neutral + bad);
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>all: {good + neutral + bad}</p>
+      <p>average: {isNaN(average) ? 0 : average}</p>
+      <p>positive: {isNaN(good / (good + neutral + bad)) ? 0 : good / (good + neutral + bad) * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -15,13 +32,7 @@ const App = () => {
       <button onClick={() => setGood(good+1)}>Good</button>
       <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
       <button onClick={() => setBad(bad+1)}>Bad</button>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>all: {good + neutral + bad}</p>
-      <p>average: {isNaN(average) ? 0 : average}</p>
-      <p>positive: {isNaN(good / (good + neutral + bad)) ? 0 : good / (good + neutral + bad) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
