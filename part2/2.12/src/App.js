9736f2eb2571c9f6fdd55a4b88c800cdc41b6ac4
IngJheyner <david-ibagon@hotmail.com>
2023-11-20 16:44:35 -0500

    Exercicies 2.13 - 2.14

--- part2/2.12/src/App.js
+++ part2/2.12/src/App.js
@@ -9,24 +9,76 @@ const Filter = ({ search, handleSearchChange }) => {
   );
 };
 
+const Weather = ({ country }) => {
+
+  const [weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_KEY);
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital[0]}`)
+      .then((response) => {
+        setWeather(response.data);
+        console.log("weather", response.data);
+      });
+  }, []);
+
+  if (weather.length === 0) {
+    return (
+      <div>
+        <p>loading weather...</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Weather in {country.capital[0]}</h3>
+        <div>
+          <b>temperature: </b> {weather.current.temperature} Celcius
+        </div>
+        <img src={weather.current.weather_icons[0]} alt="weather icon" />
+        <div>
+          <b>wind: </b> {weather.current.wind_speed} mph direction{" "}
+          {weather.current.wind_dir}
+        </div>
+      </div>
+    );
+  }
+}
+
 const Country = ({ country }) => {
   return (
     <div>
       <h2>{country.name.common}</h2>
-      <div>capital {country.capital[0]}</div>
-      <div>population {country.population}</div>
+      <div>
+        <b>capital: </b> {country.capital[0]}
+      </div>
+      <div>
+        <b>population: </b> {country.population}
+      </div>
       <h3>languages</h3>
       <ul>
         {Object.values(country.languages).map((language) => (
           <li key={language}>{language}</li>
         ))}
       </ul>
-      <img
-        src={country.flags.png}
-        height="100"
-        alt={`flag of ${country.name.common}`}
-      />
+      <img src={country.flags.png} alt="country flag" width="200" />
+      <Weather country={country} />
     </div>
+  );
+}
+
+const ShowCountry = ({ country }) => {
+  return (
+    <div>
+      <Country country={country} />
+    </div>
+  );
+}
+
+const button = (country) => {
+  return (
+    <button onClick={() => <ShowCountry country={country} />}>show</button>
   );
 }
 
@@ -38,13 +90,16 @@ const CountryList = ({ countries }) => {
         {countries.map((country) => (
           <div key={country.name.common}>
             {country.name.common}
+            {button(country)}
           </div>
         ))}
       </div>
     );
-  }else if (countries.length === 1) {
+  } else if (countries.length === 1) {
     return (
-      <Country country={countries[0]} />
+      <div>
+        <ShowCountry country={countries[0]} />
+      </div>
     );
   }
 }
@@ -78,6 +133,7 @@ function App() {
       ) : (
         <CountryList countries={countriesToShow} />
       )}
+
     </div>
   );
 }
