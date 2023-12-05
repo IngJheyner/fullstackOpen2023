4cc67f0a29be7b0da1b1f92cc537a4a5712f11eb
IngJheyner <david-ibagon@hotmail.com>
2023-12-05 16:09:49 -0500

    Exercicies 3.13 y 3.14

--- part3/intro/index.js
+++ part3/intro/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 app.use(express.json())
@@ -11,62 +13,25 @@ app.use(cors())
 app.use(express.static('build'))
 
 // Conexión a la base de datos
-const url = process.env.MONGODB_URI
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-  })
-
-const Note = mongoose.model('Note', noteSchema)
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+const Note = require('./models/note');
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  //const id = Number(request.params.id)
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
+  });
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+})
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -77,16 +42,16 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+    });
 
-  notes = notes.concat(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 
-  response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
