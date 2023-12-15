fc30ab38a5e2c455c4c28cc41c35c610e3df04ac
IngJheyner <david-ibagon@hotmail.com>
2023-12-15 18:16:30 -0500

    Exercicies 4.17

--- part4/blogs/controllers/blog.js
+++ part4/blogs/controllers/blog.js
@@ -2,19 +2,32 @@ import { Router } from "express"
 const blogsRouter = Router()
 import Blog from "../models/blog.js"
 import mongoose from "mongoose"
+import User from "../models/user.js"
 
-blogsRouter.get('/', async (req, res, next) => {
-    const blogs = await Blog.find({})
-        .catch(error => next(error))
+blogsRouter.get('/', async (req, res) => {
+
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
     res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res, next) => {
 
-    const blog = new Blog(req.body)
+    const body = req.body
+
+    const user = await User.aggregate([{ $sample: { size: 1 } }])
+
+    const blog = new Blog({
+        title: body.title,
+        url: body.url,
+        author: body.author,
+        likes: body.likes,
+        user: user[0]._id
+    })
 
     const savedBlog = await blog.save()
                         .catch(error => next(error))
+
     res.status(201).json(savedBlog)
 
 })
