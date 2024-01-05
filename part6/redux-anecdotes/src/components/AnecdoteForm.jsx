648f992d32d8f22c906f9fb6b74d553806bfb844
IngJheyner <david-ibagon@hotmail.com>
2024-01-04 19:44:44 -0500

    Exercicies 6.8

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -13,11 +13,12 @@ export const AnecdoteForm = () => {
         dispatch(newAnecdote(content))
     }
 
-    return (
+    return <>
+        <h2>create new</h2>
         <form
         onSubmit={ addAnecdote }>
             <div><input name='anecdote'/></div>
             <button type='submit'>create</button>
         </form>
-    )
+    </>
 }
