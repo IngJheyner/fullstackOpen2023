a510786afa0e7d8a7cf7785e901e03af862db85c
IngJheyner <david-ibagon@hotmail.com>
2024-01-04 19:26:42 -0500

    Exercicies 6.5

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -33,13 +33,13 @@ const reducer = (state = initialState, action) => {
             }
             return state.map(anecdote =>
                 anecdote.id !== id ? anecdote : changedAnecdote
-            )
+            ).sort((a, b) => b.votes - a.votes) // Ordena las anécdotas por número de votos
         }
         case 'NEW_ANECDOTE': {
-            return [...state, action.data]
+            return [...state, action.data].sort((a, b) => b.votes - a.votes) // Ordena las anécdotas por número de votos
         }
         default: {
-            return state
+            return state.sort((a, b) => b.votes - a.votes) // Ordena las anécdotas por número de votos
         }
     }
 }
