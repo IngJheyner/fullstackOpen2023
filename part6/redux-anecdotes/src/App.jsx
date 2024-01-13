5fdb93a6c6ca8a81bc3c6a622ec954c8ae4c960e
IngJheyner <david-ibagon@hotmail.com>
2024-01-12 20:40:12 -0500

    Exercicies 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,22 @@ import { AnecdoteForm } from './components/AnecdoteForm.jsx'
 import { AnecdoteList } from './components/AnecdoteList.jsx'
 import Filter from './components/Filter.jsx'
 import Notification from './components/Notification.jsx'
+import { useEffect } from 'react'
+
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer.js'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        anecdoteService
+            .getAll().then(anecdotes => {
+                dispatch(setAnecdotes(anecdotes))
+            })
+    } , []) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <div>
