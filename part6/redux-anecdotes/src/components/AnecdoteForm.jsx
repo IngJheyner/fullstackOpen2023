78a36b06ed7d6380609e1f40e1e0a0b887456f3b
IngJheyner <david-ibagon@hotmail.com>
2024-01-12 21:24:40 -0500

    Exercicies 6.17

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -21,7 +21,9 @@ export const AnecdoteForm = () => {
         dispatch(notificationChange(`you created '${content}'`))
 
         setTimeout(() => {
+
             dispatch(clearNotification())
+
         }, 5000)
     }
 
