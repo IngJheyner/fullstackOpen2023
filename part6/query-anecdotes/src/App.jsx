779f1116abdd3de67713789f101da98ab3088cc5
IngJheyner <david-ibagon@hotmail.com>
2024-01-16 21:39:47 -0500

    Exercicies 6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,13 +1,24 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-import { useQuery } from 'react-query'
-import { getAnecdotes } from './requests.js'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, updateAnecdote } from './requests.js'
 
 const App = () => {
 
+    const queryClient = useQueryClient()
+    const updateAnecdoteMutation = useMutation(updateAnecdote, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('anecdotes')
+        }
+    })
+
+
     const handleVote = (anecdote) => {
-        console.log('vote')
+        updateAnecdoteMutation.mutate({
+            ...anecdote,
+            votes: anecdote.votes + 1,
+        })
     }
 
 //   const anecdotes = [
@@ -20,6 +31,7 @@ const App = () => {
 
     const result = useQuery('anecdotes', getAnecdotes, {
         retry: false,
+        refetchOnWindowFocus: false,
     })
 
     if (result.isLoading) {
