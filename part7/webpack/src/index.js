7801310eb8a4e5957f35568c39b2d8146e49eebf
IngJheyner <david-ibagon@hotmail.com>
2024-02-08 16:29:42 -0500

    Se termina hasta los exercicies 7.8

---
+++ part7/webpack/src/index.js
@@ -0,0 +1,16 @@
+// import App from './App';
+
+// const hello = name => {
+//     console.log(`hello ${name}`)
+// }
+
+// App()
+
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
